9c7e99a8991634b1c7a8b1c8af70aac5706fae62
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-04-10 14:51:27 -0400

    gamemode commands

---
+++ src/main/java/com/redcraft86/redpackutils/registries/CommandRegistry.java
@@ -0,0 +1,33 @@
+package com.redcraft86.redpackutils.registries;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.GameType;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.redcraft86.redpackutils.ModClass;
+
+@Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class CommandRegistry {
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+        dispatcher.register(Commands.literal("gamemode")
+                .then(Commands.literal("1").executes(context -> setGameMode(context, GameType.CREATIVE)))
+                .then(Commands.literal("0").executes(context -> setGameMode(context, GameType.SURVIVAL)))
+                .then(Commands.literal("2").executes(context -> setGameMode(context, GameType.ADVENTURE)))
+                .then(Commands.literal("3").executes(context -> setGameMode(context, GameType.SPECTATOR))));
+    }
+
+    private static int setGameMode(CommandContext<CommandSourceStack> context, GameType type) {
+        ServerPlayer player = context.getSource().getPlayer();
+        player.setGameMode(type);
+        context.getSource().sendSuccess(() -> Component.literal("Set gamemode to " + type.getName()), true);
+        return 1;
+    }
+}
