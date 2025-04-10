95a3d63ceedeb6d4f5e8e45c304e2712a753cc74
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-04-10 15:22:42 -0400

    merge entity events

--- src/main/java/com/redcraft86/redpackutils/events/EntityEvents.java
+++ src/main/java/com/redcraft86/redpackutils/events/EntityEvents.java
@@ -1,25 +1,45 @@
 package com.redcraft86.redpackutils.events;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.entity.EntityMobGriefingEvent;
-import net.minecraftforge.eventbus.api.Event;
+import com.redcraft86.redpackutils.ModClass;
+import com.redcraft86.redpackutils.config.CommonConfig;
+import com.redcraft86.redpackutils.util.ModUtils;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.monster.Ghast;
-import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Ghast;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.projectile.Fireball;
-import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.projectile.WitherSkull;
-import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
-import com.redcraft86.redpackutils.config.CommonConfig;
-import com.redcraft86.redpackutils.ModClass;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.entity.EntityMobGriefingEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class MobGriefing {
+public class EntityEvents {
     @SubscribeEvent
-    public static void onMobGriefing(EntityMobGriefingEvent e) {
+    public static void onEntityInteract(PlayerInteractEvent.EntityInteract e) {
+        if (e.getLevel().isClientSide()) {
+            return;
+        }
+
+        if (CommonConfig.unlimitedVillager && e.getTarget() instanceof AbstractVillager villager) {
+            for (MerchantOffer offer : villager.getOffers()) {
+                offer.resetUses();
+                // We need to do some funky stuff to access them private content
+                ModUtils.setPrivateField(offer, "demand", 0);
+                ModUtils.setPrivateField(offer, "maxUses", Integer.MAX_VALUE);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onMobGrief(EntityMobGriefingEvent e) {
         Entity entity = e.getEntity();
         if (entity == null) {
             return;
