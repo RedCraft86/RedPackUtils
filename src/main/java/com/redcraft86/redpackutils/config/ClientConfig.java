0a378f2d5ce62136ec230167466e784fe239e2f2
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-04-27 14:30:06 -0400

    startup sound

--- src/main/java/com/redcraft86/redpackutils/config/ClientConfig.java
+++ src/main/java/com/redcraft86/redpackutils/config/ClientConfig.java
@@ -1,20 +1,31 @@
 package com.redcraft86.redpackutils.config;
 
 import com.redcraft86.redpackutils.ModClass;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Collections;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientConfig
 {
+
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> STARTUP_SOUNDS;
     private static final ForgeConfigSpec.BooleanValue DISABLE_CREATIVE_TAB_TIPS;
     private static final ForgeConfigSpec.BooleanValue DISABLE_GOG_STARS;
 
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     static {
         //BUILDER.push("Tweaks");
+
+        STARTUP_SOUNDS = BUILDER.comment("Picks a random sound from this list to play on startup.\nFormat is: \"sound_id volume\"")
+                .defineListAllowEmpty("startupSounds",
+                        List.of("minecraft:entity.experience_orb.pickup 0.7", "minecraft:entity.player.levelup 0.3"),
+                        obj -> obj instanceof final String name && ResourceLocation.isValidResourceLocation(name.split(" ", 2)[0]));
 
         DISABLE_CREATIVE_TAB_TIPS = BUILDER.comment("Disables the tooltip labels on Creative Mode tabs such as \"Functional Blocks,\" \"Natural Blocks,\" etc.")
                 .define("disableCreativeTabTips", true);
@@ -26,6 +37,8 @@ public class ClientConfig
     }
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 
+
+    public static List<? extends String> startupSounds = Collections.emptyList();
     public static boolean disableCreativeTabTips = true;
     public static boolean disableGoGStars = true;
 
@@ -33,6 +46,7 @@ public class ClientConfig
     static void onLoad(final ModConfigEvent event)
     {
         if (event.getConfig().getSpec() == SPEC) {
+            startupSounds = STARTUP_SOUNDS.get();
             disableCreativeTabTips = DISABLE_CREATIVE_TAB_TIPS.get();
             disableGoGStars = DISABLE_GOG_STARS.get();
         }
---
+++ src/main/java/com/redcraft86/redpackutils/events/ScreenEvents.java
@@ -0,0 +1,63 @@
+package com.redcraft86.redpackutils.events;
+
+import com.mojang.logging.LogUtils;
+import com.redcraft86.redpackutils.ModClass;
+import com.redcraft86.redpackutils.config.ClientConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Random;
+
+@Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ScreenEvents {
+    private static boolean hasPlayedStartup = false;
+    private static final Random RANDOM = new Random();
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @SubscribeEvent
+    public static void onScreenOpen(ScreenEvent.Opening event) {
+        if (event.getNewScreen() instanceof TitleScreen && !hasPlayedStartup) {
+            hasPlayedStartup = true;
+            List<? extends String> soundList = ClientConfig.startupSounds;
+            if (soundList != null && !soundList.isEmpty()) {
+                String entry = soundList.get(RANDOM.nextInt(0, soundList.size()));
+                String[] parts = entry.split(" ", 2);
+                if (parts.length != 2){
+                    LOGGER.warn("[RedPackUtils: Startup Sound] Invalid entry: {}", entry);
+                    return;
+                }
+
+                String id = parts[0].trim(), vol = parts[1].trim();
+                try {
+                    float volume = Math.max(0, Float.parseFloat(vol));
+                    if (volume <= 0){
+                        LOGGER.warn("[RedPackUtils: Startup Sound] Volume for {} is 0 or less", id);
+                        return;
+                    }
+
+                    ResourceLocation sound = new ResourceLocation(id);
+                    SoundEvent soundEvent = ForgeRegistries.SOUND_EVENTS.getValue(sound);
+                    if (soundEvent != null) {
+                        Minecraft.getInstance().getSoundManager().playDelayed(
+                                SimpleSoundInstance.forUI(soundEvent, 1, volume), 50);
+                    } else {
+                        LOGGER.error("[RedPackUtils: Startup Sound] Unknown sound: {}", id);
+                    }
+
+                } catch (NumberFormatException e) {
+                    LOGGER.error("[RedPackUtils: Startup Sound] Invalid volume number: {}", vol);
+                }
+            }
+        }
+    }
+}
