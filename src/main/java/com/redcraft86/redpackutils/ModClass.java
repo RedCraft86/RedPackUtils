966570983abe66400bb1f6c32f47f29701a15ec5
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-10 22:22:14 -0400

    cleanup

--- src/main/java/com/redcraft86/redpackutils/ModClass.java
+++ src/main/java/com/redcraft86/redpackutils/ModClass.java
@@ -3,15 +3,10 @@ package com.redcraft86.redpackutils;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraft.world.item.CreativeModeTabs;
 
@@ -32,36 +27,16 @@ public class ModClass {
         ItemRegistry.register(modEventBus);
         BlockRegistry.register(modEventBus);
 
-        modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
         context.registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
         context.registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-    }
-
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
         if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
             event.accept(BlockRegistry.RAINBOW_BEACON_BLOCK);
-        }
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
         }
     }
 }
