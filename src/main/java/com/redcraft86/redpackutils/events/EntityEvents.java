a8ab51c944ca72806b873495ce8e01064fa2d9a1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-05 17:32:42 -0400

    fancier way to do unlimited stock

--- src/main/java/com/redcraft86/redpackutils/events/EntityEvents.java
+++ src/main/java/com/redcraft86/redpackutils/events/EntityEvents.java
@@ -2,11 +2,13 @@ package com.redcraft86.redpackutils.events;
 
 import com.redcraft86.redpackutils.ModClass;
 import com.redcraft86.redpackutils.config.CommonConfig;
-import com.redcraft86.redpackutils.mixin.MerchantOfferMixin;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -22,12 +24,21 @@ public class EntityEvents {
             return;
         }
 
-        MerchantOfferMixin.resetTrades(e.getTarget());
+        Entity entity = e.getTarget();
+
+        if (CommonConfig.unlimitedVillager && entity instanceof AbstractVillager villager) {
+            for (MerchantOffer offer : villager.getOffers()) {
+                offer.resetUses();
+                offer.maxUses = Integer.MAX_VALUE;
+            }
+        }
     }
 
     @SubscribeEvent
     public static void onMobGrief(EntityMobGriefingEvent e) {
-        ResourceLocation entityID = ForgeRegistries.ENTITY_TYPES.getKey(e.getEntity().getType());
+        Entity entity = e.getEntity();
+
+        ResourceLocation entityID = ForgeRegistries.ENTITY_TYPES.getKey(entity.getType());
         if (entityID != null && CommonConfig.griefBlacklist.contains(entityID)) {
             e.setResult(Event.Result.DENY);
         }
