817a82b33764f11212dd6bac44d078f6c044b225
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-20 14:06:38 -0400

    reimplement tempt goal mixin

---
+++ src/main/java/com/redcraft86/redpackutils/mixin/TemptGoalMixin.java
@@ -0,0 +1,25 @@
+package com.redcraft86.redpackutils.mixin;
+
+import com.redcraft86.redpackutils.config.CommonConfig;
+
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// When you 'tempt' an animal with a food item and either break line of sight or hide it,
+// the animal will no longer get tempted for a set cooldown even if you bring the item back.
+// This mixin hooks into the Goal class that does that and reset the cooldown.
+@Mixin(TemptGoal.class)
+public class TemptGoalMixin {
+    @Shadow
+    private int calmDown;
+
+    @Inject(method = "stop", at = @At("TAIL"))
+    public void resetTemptCooldown(CallbackInfo ci) {
+        calmDown = 0;
+    }
+}
