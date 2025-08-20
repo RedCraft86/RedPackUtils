ca8823ad1ea1a10bee2adc20cba797d2f20a149e
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-20 12:26:46 -0400

    move

--- src/main/java/com/redcraft86/redpackutils/events/BowArrowEvents.java
+++ src/main/java/com/redcraft86/redpackutils/events/BowArrowEvents.java
@@ -1,4 +1,4 @@
-package com.redcraft86.redpackutils.patches;
+package com.redcraft86.redpackutils.events;
 
 import com.redcraft86.redpackutils.ModClass;
 
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 
 // Lets you shoot bows with Infinity without needing a single arrow in inventory
 @Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class InfinityBow {
+public class BowArrowEvents {
     @SubscribeEvent
     public static void InfinityArrows(ArrowNockEvent event) {
         ItemStack bow = event.getBow();
