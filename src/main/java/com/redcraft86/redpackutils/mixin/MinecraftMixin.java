9014d9590891b114fd7e0ed2d70c78cb9fcf9ad7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-20 13:50:23 -0400

    better window title management

---
+++ src/main/java/com/redcraft86/redpackutils/mixin/MinecraftMixin.java
@@ -0,0 +1,47 @@
+package com.redcraft86.redpackutils.mixin;
+
+import com.redcraft86.redpackutils.config.ClientConfig;
+
+import net.minecraft.client.Minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+
+    @Unique
+    private static final int UPDATE_INTERVAL = 10;
+
+    @Unique
+    private static int tickCounter = 0;
+
+    @Inject(method = "createTitle", at = @At("RETURN"), cancellable = true)
+    private void onUpdateTitle(CallbackInfoReturnable<String> cir) {
+        String result = cir.getReturnValue();
+        if (ClientConfig.titleName != null) {
+            result = ClientConfig.titleName;
+        }
+        if (ClientConfig.memUsage) {
+            Runtime runtime = Runtime.getRuntime();
+            long totalMem = runtime.totalMemory();
+            long usedMem = (totalMem - runtime.freeMemory()) / (1024 * 1024);
+            result += String.format(" | Memory: %,d MB / %,d MB (%,d MB Allocated)", usedMem,
+                    runtime.maxMemory() / (1024 * 1024), totalMem / (1024 * 1024));
+        }
+        cir.setReturnValue(result);
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void onTick(CallbackInfo ci) {
+        tickCounter++; // Slow tick
+        if (tickCounter >= UPDATE_INTERVAL) {
+            ((Minecraft)(Object)this).updateTitle();
+            tickCounter = 0;
+        }
+    }
+}
