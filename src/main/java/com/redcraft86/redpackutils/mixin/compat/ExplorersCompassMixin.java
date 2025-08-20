dae97859b08464f75fa8b0efb57f328457d17412
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-20 15:16:37 -0400

    third party fixes

---
+++ src/main/java/com/redcraft86/redpackutils/mixin/compat/ExplorersCompassMixin.java
@@ -0,0 +1,21 @@
+package com.redcraft86.redpackutils.mixin.compat;
+
+import com.chaosthedude.explorerscompass.client.ClientEventHandler;
+
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// Fix double text overlay
+@Mixin(value = ClientEventHandler.class, remap = false)
+public class ExplorersCompassMixin {
+    @Inject(method = "onRenderTick", at = @At("HEAD"), cancellable = true)
+    private void renderTick(RenderGuiOverlayEvent.Post event, CallbackInfo ci) {
+        if (!event.getOverlay().id().toString().equals("minecraft:crosshair")) {
+            ci.cancel();
+        }
+    }
+}
