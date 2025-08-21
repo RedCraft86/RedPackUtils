9fab82614f5f247eab9f50d4fea33591955c1d81
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-08-20 21:37:13 -0400

    better bonemeal

--- src/main/java/com/redcraft86/redpackutils/config/CommonConfig.java
+++ src/main/java/com/redcraft86/redpackutils/config/CommonConfig.java
@@ -26,6 +26,12 @@ public class CommonConfig {
     private static final ForgeConfigSpec.BooleanValue CAMPFIRE_CLEAR_HARM;
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> CAMPFIRE_EFFECTS;
 
+    private static final ForgeConfigSpec.BooleanValue BONEMEAL_DIRT_GRASS;
+    private static final ForgeConfigSpec.IntValue SHORT_GRASS_CHANCE;
+    private static final ForgeConfigSpec.IntValue TALL_GRASS_CHANCE;
+    private static final ForgeConfigSpec.IntValue RANDOM_FLOWER_CHANCE;
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> FLOWER_BLACKLIST;
+
     private static final ForgeConfigSpec.ConfigValue<? extends String> STRUCTURE_SPAWNPOINT;
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> SPAWN_POINT_BLACKLIST;
 
@@ -33,7 +39,7 @@ public class CommonConfig {
     static {
         GRIEF_BLACKLIST = BUILDER.comment("List of entity IDs that cannot grief the world.")
             .defineListAllowEmpty("mobGriefBlacklist", List.of("minecraft:creeper", "minecraft:enderman", "minecraft:fireball", "minecraft:wither_skull"),
-                obj -> obj instanceof String);
+                obj -> obj instanceof String id && ResourceLocation.isValidResourceLocation(id));
 
         BUILDER.push("Campfire Effects");
 
@@ -48,6 +54,25 @@ public class CommonConfig {
                 obj -> obj instanceof final String name && ResourceLocation.isValidResourceLocation(name.split(" ", 2)[0]));
 
         BUILDER.pop();
+        BUILDER.push("Better Bonemeal");
+
+        BONEMEAL_DIRT_GRASS = BUILDER.comment("Lets player bonemeal dirt blocks into grass.")
+            .define("bonemealDirtToGrass", true);
+
+        SHORT_GRASS_CHANCE = BUILDER.comment("Chance of short grass spawning from bonemeal.")
+            .defineInRange("shortGrassChance", 30, 0, 100);
+
+        TALL_GRASS_CHANCE = BUILDER.comment("Chance of tall grass spawning from bonemeal.")
+            .defineInRange("tallGrassChance", 20, 0, 100);
+
+        RANDOM_FLOWER_CHANCE = BUILDER.comment("Chance of spawning a random flower instead of air in place of grass.")
+            .defineInRange("randomFlowerChance", 75, 0, 100);
+
+        FLOWER_BLACKLIST = BUILDER.comment("List of flowers that bonemeal cannot spawn.")
+            .defineListAllowEmpty("flowerBlacklist", List.of("minecraft:wither_rose"),
+                obj -> obj instanceof String id && ResourceLocation.isValidResourceLocation(id));
+
+        BUILDER.pop();
         BUILDER.push("Spawn Structure");
 
         STRUCTURE_SPAWNPOINT = BUILDER.comment("Spawns the player in the nearest structure within a 128-chunk radius from [0, 0, 0]. (a single ID or a Tag, leave empty to disable)")
@@ -55,7 +80,7 @@ public class CommonConfig {
 
         SPAWN_POINT_BLACKLIST = BUILDER.comment("List of structure IDs to ignore when searching for the nearest valid structure spawn point. (Only used when structureID is a Tag)")
             .defineListAllowEmpty("structureBlacklist", List.of("minecraft:village_snowy"),
-                obj -> obj instanceof String);
+                obj -> obj instanceof String id && ResourceLocation.isValidResourceLocation(id));
 
         BUILDER.pop();
     }
@@ -67,6 +92,13 @@ public class CommonConfig {
     public static boolean campfireClearHarm = true;
     public static Map<MobEffect, Integer> campfireEffects = new HashMap<>();
 
+    public static boolean regenFlowerList = true;
+    public static boolean bonemealDirtGrass = true;
+    public static float shortGrassChance = 0.3f;
+    public static float tallGrassChance = 0.2f;
+    public static float randomFlowerChance = 0.75f;
+    public static Set<String> flowerBlacklist = new HashSet<>();
+
     public static String structureSpawnPoint = "#minecraft:village";
     public static Set<ResourceLocation> spawnPointBlacklist = new HashSet<>();
 
@@ -77,18 +109,27 @@ public class CommonConfig {
             return;
         }
 
-        processIdList(GRIEF_BLACKLIST.get(), griefBlacklist, "Grief Blacklist");
+        processIdListRL(GRIEF_BLACKLIST.get(), griefBlacklist, "Grief Blacklist");
 
         campfireEffRange = CAMPFIRE_EFFECT_RANGE.get();
-        campfireClearHarm = CAMPFIRE_CLEAR_HARM.get();
         if (campfireEffRange > 0) {
+            campfireClearHarm = CAMPFIRE_CLEAR_HARM.get();
             processCampfireEffects(CAMPFIRE_EFFECTS.get());
         } else {
             campfireEffects.clear();
         }
 
+        regenFlowerList = true;
+        bonemealDirtGrass = BONEMEAL_DIRT_GRASS.get();
+        shortGrassChance = Math.min(100.0f, (float)SHORT_GRASS_CHANCE.get() / 100.0f);
+        tallGrassChance = Math.min(100.0f, (float)TALL_GRASS_CHANCE.get() / 100.0f);
+        randomFlowerChance = Math.min(100.0f, (float)RANDOM_FLOWER_CHANCE.get() / 100.0f);
+        processIdListStr(FLOWER_BLACKLIST.get(), flowerBlacklist, "Better Bonemeal (Blacklist)");
+
         structureSpawnPoint = STRUCTURE_SPAWNPOINT.get();
-        processIdList(SPAWN_POINT_BLACKLIST.get(), spawnPointBlacklist, "Structure Spawn Point (Blacklist)");
+        if (ResourceLocation.isValidResourceLocation(structureSpawnPoint)) {
+            processIdListRL(SPAWN_POINT_BLACKLIST.get(), spawnPointBlacklist, "Structure Spawn Point (Blacklist)");
+        }
     }
 
     private static void processCampfireEffects(List<? extends String> entries) {
@@ -124,11 +165,22 @@ public class CommonConfig {
         }
     }
 
-    private static void processIdList(List<? extends String> entries, Set<ResourceLocation> result, String logCategory) {
+    private static void processIdListRL(List<? extends String> entries, Set<ResourceLocation> result, String logCategory) {
         result.clear();
         for (String entry : entries) {
             if (ResourceLocation.isValidResourceLocation(entry)){
                 result.add(ResourceLocation.parse(entry));
+            } else {
+                LOGGER.error("[RedPackUtils: {}] Invalid ID: {}", logCategory, entry);
+            }
+        }
+    }
+
+    private static void processIdListStr(List<? extends String> entries, Set<String> result, String logCategory) {
+        result.clear();
+        for (String entry : entries) {
+            if (ResourceLocation.isValidResourceLocation(entry)){
+                result.add(entry);
             } else {
                 LOGGER.error("[RedPackUtils: {}] Invalid ID: {}", logCategory, entry);
             }
---
+++ src/main/java/com/redcraft86/redpackutils/systems/BetterBonemeal.java
@@ -0,0 +1,133 @@
+package com.redcraft86.redpackutils.systems;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import com.redcraft86.redpackutils.ModClass;
+import com.redcraft86.redpackutils.config.CommonConfig;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.server.TickTask;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
+
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ModClass.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class BetterBonemeal {
+    private static final Random random = new Random();
+    public static List<Block> flowers = new ArrayList<Block>();
+
+    @SubscribeEvent
+    static void onServerStart(LevelEvent.Load event) {
+        LevelAccessor level = event.getLevel();
+        if (level.isClientSide()) {
+            return;
+        }
+
+        generateFlowerList((Level)level);
+    }
+
+    @SubscribeEvent // Dirt to grass
+    static void onRightClick(PlayerInteractEvent.RightClickBlock event) {
+        Level level = event.getLevel();
+        Player player = event.getEntity();
+        if (level.isClientSide() || event.getHand() != InteractionHand.MAIN_HAND || !CommonConfig.bonemealDirtGrass) {
+            return;
+        }
+
+        ItemStack item = event.getItemStack();
+        if (!item.is(Items.BONE_MEAL)) {
+            return;
+        }
+
+        BlockPos pos = event.getPos();
+        if (level.getBlockState(pos).is(Blocks.DIRT)) {
+            level.setBlockAndUpdate(pos, Blocks.GRASS_BLOCK.defaultBlockState());
+            if (!player.isCreative()) {
+                item.shrink(1);
+            }
+            player.swing(InteractionHand.MAIN_HAND, true);
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    static void OnBonemeal(BonemealEvent event) {
+        Level level = event.getLevel();
+        if (level.isClientSide() || CommonConfig.shortGrassChance >= 0.99f || CommonConfig.tallGrassChance >= 0.99f) {
+            return;
+        }
+
+        if (CommonConfig.regenFlowerList) {
+            generateFlowerList(level);
+        }
+
+        BlockPos originPos = event.getPos();
+        int x = originPos.getX();
+        int y = originPos.getY();
+        int z = originPos.getZ();
+
+        level.getServer().tell(new TickTask(level.getServer().getTickCount(), () -> {
+            Iterator<BlockPos> it = BlockPos.betweenClosedStream(x-6, y, z-6, x+6, y+1, z+6).iterator();
+            while (it.hasNext()) {
+                BlockPos pos = it.next();
+                Block block = level.getBlockState(pos).getBlock();
+                if (block == Blocks.GRASS && random.nextFloat() > CommonConfig.shortGrassChance) {
+                    level.setBlockAndUpdate(pos, airOrRandomFlower().defaultBlockState());
+                }
+                if (block == Blocks.TALL_GRASS && random.nextFloat() > CommonConfig.tallGrassChance) {
+                    // If tall grass, silently break the top block first to avoid breaking effect
+                    level.setBlock(pos.above(), Blocks.AIR.defaultBlockState(), 2 | 16);
+                    level.setBlockAndUpdate(pos, airOrRandomFlower().defaultBlockState());
+                }
+            }
+        }));
+    }
+
+    private static Block airOrRandomFlower() {
+        if (random.nextFloat() < CommonConfig.randomFlowerChance) {
+            return flowers.get(random.nextInt(flowers.size()));
+        } else {
+            return Blocks.AIR;
+        }
+    }
+
+    private static void generateFlowerList(Level level) {
+        if (level == null) {
+            return;
+        }
+
+        flowers.clear();
+        CommonConfig.regenFlowerList = false;
+
+        Registry<Block> blockRegistry = level.registryAccess().registryOrThrow(Registries.BLOCK);
+        for (Block block : blockRegistry) {
+            if (block instanceof FlowerBlock) {
+                ResourceLocation id = blockRegistry.getKey(block);
+                if (id == null || CommonConfig.flowerBlacklist.contains(id.toString())) {
+                    continue;
+                }
+
+                flowers.add(block);
+            }
+        }
+    }
+}
